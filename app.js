15ac7d0b3c4adb27d4e5502158d9163f8f16a1e3
Jay Vigilla <JayRVigilla@gmail.com>
2020-03-10 16:16:42 -0700

    finished it all, the basics

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
+$('#searchbutton').on('click', getGif)
+$('#destroy').on('click', clearDiv)
+
+async function getGif() {
+    let userSearch = $('input').val();
+    let response = await axios.get(`http://api.giphy.com/v1/gifs/search?q=${userSearch}&api_key=MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym`)
+    // console.log(response.data);
+    // console.log(response.data.data[0].url);
+    appendToDom(response.data.data[0].images.fixed_height.url)
+
+}
+
+// appends gif to DOM
+function appendToDom(gifLink) {
+    let $gifToAdd = $('<img>')
+        .attr('src', gifLink);
+    $('.allTheGif').append($gifToAdd);
+}
+
+//clear div of gifs
+function clearDiv(){
+    $('.allTheGif').empty();
+}
+
 console.log("Let's get this party started!");
