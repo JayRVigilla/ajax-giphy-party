e9d34cbbcf02662d3c634024802d526db50a7aa2
Jay Vigilla <JayRVigilla@gmail.com>
2020-03-10 17:05:54 -0700

    playing with further study

--- dadJokeapp.js
+++ dadJokeapp.js
@@ -1,7 +1,28 @@
-//
+let jokes = [];
+// axios.default.headers.common['Accept'] = 'text/plain';
+
+let config = {
+    headers: {
+        header1: 'application/json'
+    }
+};
+
+
+async function dadJokes() {
+    for (let i = 0; i < 10; i++) {
+        let joke = await axios.get('https://icanhazdadjoke.com/');
+        jokes.push(joke);
+    }
+    console.log(jokes);
+}
+dadJokes();
+
+
+
 
 // calculate net score()
 
 // vote up button 
 // vote down button 
 // netscoreboard
+
